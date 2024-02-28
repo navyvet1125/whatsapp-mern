d408b2f73d541f8a2bf201671ca7c01fdd14c163
Evan Washington <enavy04@gmail.com>
2024-02-27 21:47:20 -0800

    Added style to Chat.css and Sidebar.css.  Imported Material-UI. added Sidebar functionality.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,12 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
       <h1> I am a Sidebar</h1>
+      <div className='sidebar__header'>
+        <div className='sidebar__headerRight'>
+            
+        </div>
+      
+      </div>
     </div>
   )
 }
