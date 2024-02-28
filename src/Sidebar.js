12e3df6eddbfedf3844808cb0765b42d6e01970b
Evan Washington <enavy04@gmail.com>
2024-02-27 22:16:12 -0800

    added Material UI functionality to Sidebar.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import "./Sidebar.css";
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { Avatar, IconButton } from '@mui/material';
+import { MoreVert } from '@mui/icons-material';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <h1> I am a Sidebar</h1>
       <div className='sidebar__header'>
+        <Avatar src ='https://avatars.githubusercontent.com/u/17504105?v=4'/>
         <div className='sidebar__headerRight'>
-            
+            <IconButton>
+                <DonutLargeIcon />
+            </IconButton>
+            <IconButton>
+                <ChatIcon />
+            </IconButton>
+            <IconButton>
+                <MoreVert />
+            </IconButton>
+        
         </div>
       
+      </div>
+      <div className='sidebar__search'>
       </div>
     </div>
   )
