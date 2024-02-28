6ee3913bf025ae30231820ad5f5336fe475b86bd
Evan Washington <enavy04@gmail.com>
2024-02-27 21:35:24 -0800

    Added Chat and Sidebar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a MERN WhatsApp clone</h1>
+      <div className ="app__body">
+        <Sidebar />
+        <Chat />
+      </div>  
     </div>
   );
 }
