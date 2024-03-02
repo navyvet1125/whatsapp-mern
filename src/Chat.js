3f4f89a074caf22d4fd38ae4b3039a108a9ef2c5
Evan Washington <enavy04@gmail.com>
2024-03-01 23:10:34 -0800

    added functionality to Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import "./Chat.css"
+import { Avatar, IconButton } from '@mui/material';
+import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
 
 const Chat = () => {
   return (
     <div className='chat'>
-        <h1>I am the chat</h1>     
+        <div className='chat__header'>
+          <Avatar />
+          <div className='chat__headerInfo'>
+            <h3>Room name</h3>
+            <p>Last seen at...</p>
+          </div>
+          <div className='chat__headerRight'>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+          </div>
+        </div>   
     </div>
   )
 }
