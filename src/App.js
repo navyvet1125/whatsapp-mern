10c0f16a2f275639b942100d730ae1ba6a72935f
Evan Washington <enavy04@gmail.com>
2024-03-04 21:49:43 -0800

    Added pusher functionality to client side application.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher(process.env.REACT_APP_PUSHER_WHATSAPP_MERN_KEY, {
+      cluster: process.env.REACT_APP_PUSHER_WHATSAPP_MERN_CLUSTER
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => alert(JSON.stringify(data)));
+  }, []);
   return (
     <div className="app">
       <div className ="app__body">
